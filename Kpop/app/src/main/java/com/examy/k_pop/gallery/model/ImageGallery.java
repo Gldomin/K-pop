82b7da05bc94e7e1c3bd0c4695fa14da80bcd8ad
MrDangl <glebdangl@gmail.com>
2020-09-15 16:56:04 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/Gldomin/K-pop""
    This reverts commit 515be5f30b27693fd6aa6d6c97bb77df01195a18.

---
+++ Kpop/app/src/main/java/com/examy/k_pop/gallery/model/ImageGallery.java
@@ -0,0 +1,42 @@
+package com.examy.k_pop.gallery.model;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+/**
+ * Модель элментов для галлереи
+ */
+public class ImageGallery implements Serializable {
+    private String name;
+    private String timestamp;
+
+    public ImageGallery(String name) {
+        this.name = name;
+    }
+
+    public ImageGallery(String name, String timestamp) {
+        this.name = name;
+        this.timestamp = timestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Uri getUri() {
+        return Uri.parse("file:///android_asset/Groups/" + name);
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
