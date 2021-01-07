f10b8d2534c1a5c1a6f586f9fb515489ad6210e8
MrPunisher001 <62207280+MrPunisher001@users.noreply.github.com>
2021-01-07 18:29:05 +0800

    Смена темы

--- Kpop/app/src/main/java/com/star/k_pop/GuessBands.java
+++ Kpop/app/src/main/java/com/star/k_pop/GuessBands.java
@@ -1,6 +1,7 @@
 package com.star.k_pop;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
@@ -12,12 +13,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -42,6 +45,8 @@ public class GuessBands extends AppCompatActivity {
     int fastscore = 0;
     private boolean longnazh = false;
     private SharedPreferences spBands;
+    SharedPreferences sp;
+    OptionsSet tempSettingsSet = new OptionsSet(false, false);
 
     //Создаем лист для кнопок
     private List<Button> buttons;
@@ -69,13 +74,20 @@ public class GuessBands extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setTheme(R.style.AppTheme);
+        sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
 
+        String nameOfStorage = "settings";
+        Storage storage = new Storage(this);
+        tempSettingsSet.darkMode = storage.getBoolean(nameOfStorage, "darkMode");
+        if (tempSettingsSet.darkMode==true)setTheme(R.style.AppTheme2);
 
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_guess_bands);
-        final TextView scoreText = findViewById(R.id.scoreBands);
-        final TextView fastscoreText = findViewById(R.id.fastscoreBands);
+
+        final TextView scoreText = findViewById(R.id.scoreBands); //рекорд
+        final TextView fastscoreText = findViewById(R.id.fastscoreBands); //текущий счет
         final TextView info = findViewById(R.id.info);
 
 
@@ -119,7 +131,9 @@ public class GuessBands extends AppCompatActivity {
 
         //Подсказка названия группы на время разработки
         //надо будет удалить на релизе
+
         info.setText(band);
+
 
         //устанавливаем слушатель на основные кнопки
         OnClickListener clkGr = new OnClickListener() {
