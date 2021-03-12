742d668e6aabbc5a9e1509fcc318ef5e73e3d890
MrPunisher001 <62207280+MrPunisher001@users.noreply.github.com>
2021-03-13 00:13:44 +0800

    2 активити гесбенд

---
+++ Kpop/app/src/main/java/com/star/k_pop/activity/GuessBandsTwo.java
@@ -0,0 +1,15 @@
+package com.star.k_pop.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.star.k_pop.R;
+
+public class GuessBandsTwo extends AppCompatActivity {
+
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_bands_2);
+    }
+}
